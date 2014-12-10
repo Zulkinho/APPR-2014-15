843fc4852dcf3c0dc0c5b443899ecde796d74ab6
unknown <jure.zukanovic@student.fmf.uni-lj.si>
2014-12-10 12:02:09 +0100

    Upošteval sem navodila profesorja, ki mi jih je poslal na github in popravil prve stolpe v tabelah, dodal sem se prvo vrstico v csv podatkih, ki smo jih učereaj ponesreci zbrisali s profesorjem na vajah.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -5,8 +5,8 @@
 uvozitabelaigralcev <- function() {
   return(read.csv("podatki/tabelanogometasev.csv",
                   skip=0,
-                  header=FALSE,
-                   col.names=c('IME NOGOMETASA','DRŽAVA','POZICIJA','LETNICE.DELOVANJA','NASTOPI','ZADETKI'),
+                  header=TRUE,
+                  row.names = 1,
                   na.strings = "-",
                    fileEncoding = "Windows-1250"))
 }
@@ -24,109 +24,8 @@ STATUSS[NASTOPI >=150 & NASTOPI<300]<-'Izkušenj Arsenalovec'
 STATUSS[NASTOPI >=300]<-'Arsenalova legenda'
 STATUS<-factor(STATUSS,levels=kategorije,ordered=TRUE)
 detach(nogometasi)
-dodatenstolpec<-data.frame(STATUS)
-NOGOMETASI<-merge(nogometasi,dodatenstolpec, by = 0,all=TRUE)
-NOGOMETASI<- NOGOMETASI[-1]
-rownames(NOGOMETASI) <- NULL
-
-#dodatna tabela
-attach(NOGOMETASI)
-pozicija<-c(table(POZICIJA))
-imenapozicij<-names(pozicija)
-
-prevodi<-character(0)
-prevodi[imenapozicij="CB"]='Osrednji branilec'
-prevodi[imenapozicij="CB/LB"]='Osrednji branilec/Levi bočni branilec'
-prevodi[imenapozicij="CB/MF"]='Osrednji branilec/Vezist'
-prevodi[imenapozicij="FW"]='Napadelec'
-prevodi[imenapozicij="FW/MF"]='Napadelec/Vezist'
-prevodi[imenapozicij="FW/RW"]='Napadelec/Desni bočni napadalec'
-prevodi[imenapozicij="GK"]='Vratar'
-prevodi[imenapozicij="LB"]='Levi bočni branilec'
-prevodi[imenapozicij="LB/MF"]='Levi bočni branilec/Vezist'
-prevodi[imenapozicij="LW"]='Levi bočni napadalec'
-prevodi[imenapozicij="LW/MF"]='Levi bočni napadalec/Vezist'
-prevodi[imenapozicij="MF"]='Vezist'
-prevodi[imenapozicij="RB"]='Desni bočni branilec'
-prevodi[imenapozicij="RB/LB"]='Desni bočni branilec/Levi bočni branilec'
-prevodi[imenapozicij="RB/MF"]='Desni bočni branilec/Vezist'
-prevodi[imenapozicij="RW"]='Desni bočni napadalec'
-prevodi[imenapozicij="RW/FW"]='Desni bočni napadalec/Napadelec'
-prevodi[imenapozicij="RW/LW"]='Desni bočni napadalec/Levi bočni napadalec'
-prevodi[imenapozicij="RW/MF"]='Desni bočni napadalec/Vezist'
-
-daljšizapis<-character(0)
-daljšizapis[imenapozicij="CB"]='Centre back'
-daljšizapis[imenapozicij="CB/LB"]='Centre back/Left back'
-daljšizapis[imenapozicij="CB/MF"]='Centre back/Midfielder'
-daljšizapis[imenapozicij="FW"]='Forward'
-daljšizapis[imenapozicij="FW/MF"]='Forward/Midfielder'
-daljšizapis[imenapozicij="FW/RW"]='Forward/Right winger'
-daljšizapis[imenapozicij="GK"]='Goalkeeper'
-daljšizapis[imenapozicij="LB"]='Left back'
-daljšizapis[imenapozicij="LB/MF"]='Left back/Midfielder'
-daljšizapis[imenapozicij="LW"]='Left winger'
-daljšizapis[imenapozicij="LW/MF"]='Left winger/Midfielder'
-daljšizapis[imenapozicij="MF"]='Midfielder'
-daljšizapis[imenapozicij="RB"]='Right back'
-daljšizapis[imenapozicij="RB/LB"]='Right back/Left back'
-daljšizapis[imenapozicij="RB/MF"]='Right back/Midfielder'
-daljšizapis[imenapozicij="RW"]='Right winger'
-daljšizapis[imenapozicij="RW/FW"]='Right winger/Forward'
-daljšizapis[imenapozicij="RW/LW"]='Right winger/Left winger'
-daljšizapis[imenapozicij="RW/MF"]='Right winger/Midfielder'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
-
-attach(nogometasi)
-kategorije<-c('Začetnik v Arsenalu','Izkušenj Arsenalovec','Arsenalova legenda')
-STATUSS<-character(nrow(nogometasi))
-STATUSS[NASTOPI <150]<-'Začetnik v Arsenalu'
-STATUSS[NASTOPI >=150 & NASTOPI<300]<-'Izkušenj Arsenalovec'
-STATUSS[NASTOPI >=300]<-'Arsenalova legenda'
-STATUS<-factor(STATUSS,levels=kategorije,ordered=TRUE)
-detach(nogometasi)
-dodatenstolpec1<-data.frame(STATUS)
-NOGOMETASI<-merge(nogometasi,dodatenstolpec1, by = 0,all=TRUE)
-NOGOMETASI<- NOGOMETASI[-1]
-rownames(NOGOMETASI) <- NULL
+NOGOMETASI<-data.frame(nogometasi, STATUS)
+rm("STATUS")
 
 #dodatna tabela
 attach(NOGOMETASI)
@@ -179,8 +78,8 @@ golipopozicijah<-sapply(imenapozicij, function(x) sum(NOGOMETASI[NOGOMETASI["POZ
 nastopipopozicijah<- sapply(imenapozicij, function(x) sum(NOGOMETASI[NOGOMETASI["POZICIJA"] == x, "NASTOPI"]))
 povprecjegolovnapozicijo<-golipopozicijah/nastopipopozicijah
 detach(NOGOMETASI)
-PODTABELAPOPOZICIJAH<-data.frame(POZICIJA=imenapozicij,POPOLNO.IME=daljšizapis,PREVOD=prevodi,ŠTEVILO.IGRALCEV=pozicija,NASTOPI.PO.POZICIJAH=nastopipopozicijah, ZADETKI.PO.POZICIJAH=golipopozicijah,POVREČJE.ZADETKOV.NA.ŠTEVILO.NASTOPOV=povprecjegolovnapozicijo)
-rownames(PODTABELAPOPOZICIJAH) <- NULL
+PODTABELAPOPOZICIJAH<-data.frame(POPOLNO.IME=daljšizapis,PREVOD=prevodi,ŠTEVILO.IGRALCEV=pozicija,NASTOPI.PO.POZICIJAH=nastopipopozicijah, ZADETKI.PO.POZICIJAH=golipopozicijah,POVREČJE.ZADETKOV.NA.ŠTEVILO.NASTOPOV=povprecjegolovnapozicijo)
+
 
 # Uvoz s spletne strani Wiki
 
@@ -228,7 +127,7 @@ statuss<-character(nrow(arsenal))
 statuss[Appearances <150]<-'Beginner'
 statuss[Appearances >=150 & Appearances<300]<-'Grown up'
 statuss[Appearances >=300]<-'Legend'
-status<-factor(statuss,levels=kategorija,ordered=TRUE)
+Status<-factor(statuss,levels=kategorija,ordered=TRUE)
 detach(arsenal)
-ARSENAL<-data.frame(arsenal,status)
+ARSENAL<-data.frame(arsenal,Status)
 
