e1925483d9fe0e8f272ee1b0b39a4268d098563a
Jure Zukanovič <jure.zuka.jz@gmail.com>
2015-01-02 15:50:21 +0100

    izbrisal sem tabelo druzine v podtkih saj je neuporabna, s pomočjo PODTABELAPOPOZICIJAH sem naredil in uvozil v CSV obliki še eno podtabelo UREJENAPODTABELA, ki je enaka kot PODTABELAPOPOZICIJAH le da je namesto po abecedi urejena po igralnih mestih. spremenil sem tudi graf, ki sedaj vleče podatke iz UREJENAPODTABELA

--- slike/grafi.r
+++ slike/grafi.r
@@ -1,4 +1,4 @@
 pdf("slike/grafi.pdf",paper="a4")
-barplot(PODTABELAPOPOZICIJAH[1:19,'NASTOPI.PO.POZICIJAH'], names.arg = imenapozicij,xlab="POZICIJA", ylab="ŠTEVILO NASTOPOV", main= 'ŠTEVILO NASTOPOV GLEDE NA POZICIJO', las=2, cex.names=0.75, col="red")
+barplot(UREJENAPODTABELA[1:19,'NASTOPI.PO.POZICIJAH'], names.arg = imenapozicij,xlab="POZICIJA", ylab="ŠTEVILO NASTOPOV", main= 'ŠTEVILO NASTOPOV GLEDE NA POZICIJO', las=2, cex.names=0.75, col="red")
 dev.off()
 
