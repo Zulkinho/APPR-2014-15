d86039d19c2a209535870286d1e09d3d629ab223
unknown <jure.zukanovic@student.fmf.uni-lj.si>
2015-02-24 18:03:08 +0100

    uvozil sem podatke o igralcih, ki jih bom analiziral v 4. fazi

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,17 +1,37 @@
-# 4. faza: Analiza podatkov
+#uvozim tabelo
+ZADNJIH50<-read.csv2("podatki/zadnjih50.csv",
+                           skip=0,
+                           row.name=1,
+                           header=TRUE,
+                           na.strings = "-",
+                           fileEncoding = "Windows-1252")
 
-# Uvozimo funkcijo za uvoz spletne strani.
-source("lib/xml.r")
 
-# Preberemo spletno stran v razpredelnico.
-cat("Uvažam spletno stran...\n")
-tabela <- preuredi(uvozi.obcine(), obcine)
 
-# Narišemo graf v datoteko PDF.
-cat("Rišem graf...\n")
-pdf("slike/naselja.pdf", width=6, height=4)
-plot(tabela[[1]], tabela[[4]],
-     main = "Število naselij glede na površino občine",
-     xlab = "Površina (km^2)",
-     ylab = "Št. naselij")
-dev.off()
+
+
+
+
+
+
+
+
+
+
+# # 4. faza: Analiza podatkov
+# 
+# # Uvozimo funkcijo za uvoz spletne strani.
+# source("lib/xml.r")
+# 
+# # Preberemo spletno stran v razpredelnico.
+# cat("Uvažam spletno stran...\n")
+# tabela <- preuredi(uvozi.obcine(), obcine)
+# 
+# # Narišemo graf v datoteko PDF.
+# cat("Rišem graf...\n")
+# pdf("slike/naselja.pdf", width=6, height=4)
+# plot(tabela[[1]], tabela[[4]],
+#      main = "Število naselij glede na površino občine",
+#      xlab = "Površina (km^2)",
+#      ylab = "Št. naselij")
+# dev.off()
