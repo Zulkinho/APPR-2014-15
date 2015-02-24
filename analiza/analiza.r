d134a6575edbbccdc0f3233f71dd67f92bd0ed75
Jure Zukanovič <jure.zuka.jz@gmail.com>
2015-02-24 21:26:58 +0100

    analiziral sem zadnjih 50 igralcev, kateri 3je so najboljši nasplošno in kateri je bil najaboljši v Arsenalu

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -7,12 +7,69 @@ ZADNJIH50<-read.csv2("podatki/zadnjih50.csv",
                            fileEncoding = "Windows-1252")
 
 
+#razdelitev v 4 skupine glede na kariero v vseh pomenih, iščemo kdo je bil najboljši
+normaliziran <- scale(as.matrix(ZADNJIH50[c(4:5, 7:13)]))
+matrikarazdalj<-dist(normaliziran)
+razdelitev<- hclust(matrikarazdalj, method = "complete")
+
+pdf("slike/najboljsi.pdf")
+plot(razdelitev, hang=-1, cex=0.6, main = "ZADNJIH50")
+rect.hclust(razdelitev,k=4,border="red")
+dev.off()
+#iz grafa vidimo kdo sta nogometaša, ki sta se v svoji karieri najbol izkazala ter na vseh področjih kot celoti dosegla najboljše rezultate
+#to sta Henry in Bergkamp
+#preverimo če to res drži
+
+p <- cutree(razdelitev, k=4)
+barve=c("red", "green", "blue","yellow")
+table(p)
+barve
+
+pdf("slike/najboljsi1.pdf")
+pairs(normaliziran, col = barve[p])
+dev.off()
+#graf prikazuje položaje osamelcev na pram ostalim
+#vidimo da sta to rdeča krogca
+
+ZADNJIH50[p %in% c(1),]
+#potrditev, da Henry in Bergkamp najbol izstopata, sta osamelca 
 
 
+#sedaj nas pa zanima kdo je 3. po dosežkih v svoji karieri neglede na klub
+# da ga najdemo uporabimo metodo enojnega povezovanja in graf razdelimo na 4 skupine
 
+razdelitev1 <- hclust(matrikarazdalj, method = "single")
 
+pdf("slike/najboljsi2.pdf")
+plot(razdelitev1, hang=-1, cex=0.6, main = "ZADNJIH50")
+rect.hclust(razdelitev1,k=4,border="red")
+dev.off()
+#iz grafa vidimo, da je to Robin van Persie, poleg pa graf še dodatno potrjuje katera dva sta najboljša
 
+#tako lahko trdimo, da so trije najboljši igralci med zadnjimi pedesetimi Henry,Bergkamp in van Persie
 
+#poglejmo si sedaj kdo pa je bil najboljši za Arsenal
+#normiliziral bom sedaj le dolžino kariere v Arsenalu, zadetke za Arsenal, ter nastope za Arsenal saj je to le bistvo projekta(analiza Arsenala)
+norm <- scale(as.matrix(ZADNJIH50[c(7:8, 10)]))
+matrikarazdalj1<-dist(norm)
+razdelitev2 <- hclust(matrikarazdalj1, method = "complete")
+
+pdf("slike/najboljsiArsenal.pdf")
+plot(razdelitev2, hang=-1, cex=0.6, main = "ZADNJIH50")
+rect.hclust(razdelitev2,k=4,border="red")
+dev.off()
+#Graf trdi da najbol izstopa Henry, poglejmo če to drži
+
+p1<- cutree(razdelitev2, k=4)
+table(p1)
+barve
+pdf("slike/najboljsiArsenal1.pdf")
+pairs(normaliziran, col = barve[p1])
+dev.off()
+#vidimo, da najbolj odstopa rumeni krogec(je osamelec)
+
+ZADNJIH50[p1 %in% c(4),]
+#potrditev da je najboljši igralec Arsenala med zadnjimi 50 igralci Thierry Henry, ki tudi v splošnem velja za najboljšega igralca vseh časov v klubu
 
 
 
